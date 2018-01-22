d033132c37bacbc8b8b8bd5d8a633f3c059974f3
ahmedtaaw <ahmedtaaw@gmail.com>
2018-01-22 20:50:40 +0200

    update

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
- 
+ var Person = function(name){
+    this.personName = name;
+ };
+
+ Person.prototype.sayName=function(){
+     console.log('hi my name is: '+this.personName);
+ };
+Person.prototype.shoutName=function(){
+    console.log('Hi my name is: '+this.personName+'!');
+};
+
+ var ahmed = new Person("ahmed's name");
+ var mina = new Person("mina's name");
+
+ ahmed.sayName();
+ mina.shoutName();
+
